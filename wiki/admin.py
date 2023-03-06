7757f8ffd7d0eb670b7f77f7485ccfca3d619466
Paul Bailey <paul@neutron.studio>
2023-03-06 16:59:58 -0600

    start wiki

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from wiki.models import Wiki, WikiGroup, WikiMember, Page, Version
+
+
+@admin.register(Wiki)
+class WikiAdmin(admin.ModelAdmin):
+  list_display = ('name', 'org', 'modified')
+  list_filter = ('modified',)
+  search_fields = ('name',)
+  date_hierarchy = 'modified'
+  raw_id_fields = ('org',)
+
+
+class MemberInline(admin.TabularInline):
+  model = WikiMember
+  raw_id_fields = ('user',)
+
+
+@admin.register(WikiGroup)
+class WikiGroupAdmin(admin.ModelAdmin):
+  list_display = ('name', '_paths', 'wiki', 'modified')
+  list_filter = ('modified',)
+  search_fields = ('name', 'wiki__name')
+  raw_id_fields = ('wiki',)
+  inlines = [MemberInline]
+
+  def _paths(self, obj):
+    if obj:
+      return mark_safe('<br>'.join(obj.paths))
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+  list_display = ('path', 'wiki', 'org', 'modified')
+  list_filter = ('modified',)
+  date_hierarchy = 'modified'
+  search_fields = ('name', 'wiki__name')
+  raw_id_fields = ('wiki',)
+
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+  list_display = ('path', 'wiki', 'org', 'publish_on', 'modified')
+  list_filter = ('modified',)
+  date_hierarchy = 'modified'
+  search_fields = ('page__path', 'page__path__wiki__name')
+  raw_id_fields = ('page', 'approved_by', 'created_by', 'modified_by')
--- wiki/models.py
+++ wiki/models.py
@@ -1,11 +1,16 @@
+import re
+
 from django.db import models
 from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import ValidationError
 
 
 class Wiki(models.Model):
   name = models.CharField(max_length=150)
   slug = models.SlugField(unique=True)
+
+  org = models.ForeignKey('account.Organization', on_delete=models.CASCADE)
 
   created = models.DateTimeField(auto_now_add=True)
   modified = models.DateTimeField(auto_now=True)
@@ -62,8 +67,19 @@ class WikiMember(models.Model):
     return self.user.email
 
 
+def validate_path(value):
+  if not value.startswith("/"):
+    raise ValidationError("Paths must start with /", code="invalid", params={"value": value})
+
+  if value.endswith("/"):
+    raise ValidationError("Paths can not end with /", code="invalid", params={"value": value})
+
+  if re.search(r"^[-a-z0-9/]+\Z") is None:
+    raise ValidationError("Paths can only container lowercase letters, numbers, and dashes", code="invalid", params={"value": value})
+
+
 class Page(models.Model):
-  path = models.CharField(max_length=150)
+  path = models.CharField(max_length=150, validators=[validate_path])
 
   wiki = models.ForeignKey(Wiki, on_delete=models.CASCADE)
 
@@ -73,6 +89,10 @@ class Page(models.Model):
   class Meta:
     ordering = ['path']
     unique_together = [['path', 'wiki']]
+
+  @property
+  def org(self):
+    return self.wiki.org
 
 
 class Version(models.Model):
@@ -92,3 +112,15 @@ class Version(models.Model):
 
   def __str__(self):
     return str(self.page)
+
+  @property
+  def path(self):
+    return self.page.path
+
+  @property
+  def wiki(self):
+    return self.page.wiki
+
+  @property
+  def org(self):
+    return self.page.wiki.org
